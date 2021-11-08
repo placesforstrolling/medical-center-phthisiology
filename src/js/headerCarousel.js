acee7e675c7e27ef2a794de69cca25f5bb8674d9
Must <mustafaanvarov@mail.ru>
2021-11-08 11:57:32 +0500

    Fix history page bg image and changed the structure of footer

--- src/js/headerCarousel.js
+++ src/js/headerCarousel.js
@@ -2,8 +2,10 @@ window.addEventListener('load', () => {
 
     const carouselItem = $('header .carousel-item'),
           underMenuHei = $('header .under-menu-in').outerHeight(),
+          headerImage = $('header .header-background'),
           headerHeight = $('header').outerHeight(),
           heightForCarousel = headerHeight - underMenuHei;
     
     carouselItem.height(heightForCarousel);
+    headerImage.height(heightForCarousel);
   });
